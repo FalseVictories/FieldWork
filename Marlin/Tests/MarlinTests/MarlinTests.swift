3f0540aab6c7f638b832cd76d50fc98ec4f939a1
iain <iain@falsevictories.com>
2025-11-20 23:39:07 +0000

    Add a test suite, well, a test.
    Add a test to check the data returned from the sample matches the data added to the sample.

--- Marlin/Tests/MarlinTests/MarlinTests.swift
+++ Marlin/Tests/MarlinTests/MarlinTests.swift
@@ -1,6 +1,38 @@
+import Foundation
 import Testing
 @testable import Marlin
 
-@Test func example() async throws {
+@MainActor
+func makeTestSample() throws -> Sample {
+    if let sample = Sample() {
+        let channel = SampleChannel(withSampleBlockFactory: try DefaultSampleBlockFactory())
+        
+        let data = UnsafeMutableBufferPointer<Float>.allocate(capacity: 44100)
+        for i in 0..<44100 {
+            data[i] = sin(Float(i) * Float.pi / 180)
+        }
+        
+        try channel.appendData(UnsafeBufferPointer(data))
+        sample.channels.append(channel)
+        return sample
+    }
+    fatalError()
+}
+
+@MainActor
+@Test func testSampleDataSmall() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    let testSample = try makeTestSample()
+    #expect(testSample.numberOfFrames == 44100)
+    
+    let block = testSample.channels[0].firstBlock
+    #expect(block != nil)
+    
+    guard let block else {
+        fatalError()
+    }
+  
+    for i in 0..<44100 {
+        #expect(block.data(at: UInt64(i)) == sin(Float(i) * Float.pi / 180))
+    }
 }
