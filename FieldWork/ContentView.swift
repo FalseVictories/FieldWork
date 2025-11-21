bf4b042b8d7e895508f326482afbeddb690a3f60
iain <iain@falsevictories.com>
2025-11-21 23:27:22 +0000

    Add display the sample info after it's loaded

--- FieldWork/ContentView.swift
+++ FieldWork/ContentView.swift
@@ -2,8 +2,9 @@ import Marlin
 import SwiftUI
 
 struct ContentView: View {
-    static let sampleUrl = URL(filePath: "/Users/iain/Music/Logic/Bounces/example-right-channel-stereo.wav")
+    static let sampleUrl = URL(filePath: "/Users/iain/Music/hidden.flac")
     @State var sample: Sample
+    @State var text: String = ""
     
     init() {
         if let s = Sample() {
@@ -23,6 +24,14 @@ struct ContentView: View {
             if let operation = sample.currentOperation {
                 ProgressView(operation.title ?? "",
                              value: operation.progress, total: 1.0)
+            }
+            
+            if sample.numberOfFrames > 0 {
+                Text("Filename: \(Self.sampleUrl.lastPathComponent)")
+                Text("Channels: \(sample.channels.count == 1 ? "Mono" : "Stereo")")
+                Text("Number of Frames: \(sample.numberOfFrames)")
+                Text("Sample Rate: \(Int(sample.sampleRate))")
+                Text("Bit Depth: \(sample.bitDepth)")
             }
         }
         .padding()
